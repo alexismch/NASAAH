3a7d2f6f8fdf6b4c09faed841dac5e320862f992
PC-PORN-MCH\priva <private@alexismch.be>
2020-11-03 23:16:04 +0100

    ma maman la salope

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Movement : MonoBehaviour
+namespace Script
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Movement : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
