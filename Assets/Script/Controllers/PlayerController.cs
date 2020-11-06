de2ac2913f6acd02fdef8d88672effcdc501fd09
amaurydetremerie <amaury.detremerie@gmail.com>
2020-11-06 16:49:15 +0100

    AJout des script de movement

---
+++ Assets/Script/Controllers/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Script.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
