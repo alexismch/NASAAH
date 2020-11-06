1564d5107565cc9a2507cb90785d41210ce9e00e
amaurydetremerie <amaury.detremerie@gmail.com>
2020-11-07 00:47:42 +0100

    Ajout de la mort du player en cas de collision avec une kkhuaite

---
+++ Assets/Script/Collisions/OnCollisionWithEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Script.Manager;
+using UnityEngine;
+
+namespace Script.Collisions
+{
+    public class OnCollisionWithEnemy : MonoBehaviour
+    {
+        [SerializeField] private LayerMask layerMask;
+        
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if ((layerMask & 1 << other.gameObject.layer) == 1 << other.gameObject.layer)
+            {
+                GameManager.Kill();
+            }
+        }
+        
+        
+    }
+}
