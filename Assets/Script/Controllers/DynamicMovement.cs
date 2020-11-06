e9ed0b194291e986c83c814b7affdc7722c4e9a1
amaurydetremerie <amaury.detremerie@gmail.com>
2020-11-06 17:02:02 +0100

    Fin du DynamicMovement.cs

--- Assets/Script/Controllers/DynamicMovement.cs
+++ Assets/Script/Controllers/DynamicMovement.cs
@@ -2,8 +2,33 @@
 
 namespace Script.Controllers
 {
-    public class DynamicMovement : MonoBehaviour
+    
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class DynamicMovement : Movement
     {
+        private float _horizontal;
+        private float _vertical;
+        private Rigidbody2D _rigidbody;
         
+        [SerializeField] private float speed;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+        
+        public override void Move(float horizontal, float vertical)
+        {
+            _horizontal = horizontal;
+            _vertical = vertical;
+        }
+        
+        private void FixedUpdate()
+        {
+            var velocity = _rigidbody.velocity;
+            velocity.x = _horizontal * speed;
+            velocity.y = _vertical * speed;
+            _rigidbody.velocity = velocity;
+        }
     }
 }
