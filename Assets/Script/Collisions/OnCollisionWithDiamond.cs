932fbc975e94eaea4c60278979cbcb1875c74333
Huy <kh.nguyen.huy@gmail.com>
2020-11-06 22:28:07 +0100

    detect player

--- Assets/Script/Collisions/OnCollisionWithDiamond.cs
+++ Assets/Script/Collisions/OnCollisionWithDiamond.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 namespace Script.Collisions
 {
     public class OnCollisionWithDiamond : MonoBehaviour
+    
     {
         [SerializeField] private UnityEvent onDestroyed;
+        [SerializeField] private LayerMask layerMask;
 
         
         private void OnTriggerEnter2D(Collider2D other)
         {
+            
             Debug.Log("yo");
-            onDestroyed?.Invoke();
-            Destroy(transform.gameObject);
+            if ((layerMask & 1 << other.gameObject.layer) == 1 << other.gameObject.layer)
+            {
+                onDestroyed?.Invoke();
+                Destroy(transform.gameObject);
+            }
+            
         }
 
         public enum Effects
