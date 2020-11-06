e8a447d42237a6282bd02b60f42cb7046795189d
Huy <kh.nguyen.huy@gmail.com>
2020-11-06 22:58:18 +0100

    turret roation

--- Assets/Script/Controllers/TurretController.cs
+++ Assets/Script/Controllers/TurretController.cs
@@ -9,26 +9,15 @@ namespace Script.Controllers
     {
         [SerializeField] private MoveToTarget moveToTarget;
         [SerializeField] private GameManager gameManager;
-        [SerializeField] private float rotationSpeed;
-        [SerializeField] private float maxAngleRotation;
         [SerializeField] private LayerMask layerMask;
+        private bool _isMoving;
 
-        private Transform _transform;
-        private int clockwiseRotation = 1;
-        private float _initialRotation;
-
-        private void Awake()
-        {
-            _transform = transform;
-            _initialRotation = _transform.rotation.z;
-        }
-
-     
         private void OnTriggerEnter2D(Collider2D other)
         {
             
             if ((layerMask & 1 << other.gameObject.layer) == 1 << other.gameObject.layer)
             {
+                _isMoving = true;
                 var player = gameManager.Player;
                 moveToTarget.SetTarget(player);
             }
@@ -37,13 +26,15 @@ namespace Script.Controllers
 
         private void Update()
         {
-            //détecter quand on est au bout de la rotation
-            if (Math.Abs(_transform.rotation.z - (_initialRotation + maxAngleRotation)) == 0 || Math.Abs(_transform.rotation.z - (_initialRotation - maxAngleRotation)) == 0)
+            if (!_isMoving)
             {
-                //Définir le sens de rotation. Quand on arrive au bout, on fait clockwiseRotation = -clockwiseRotation
-                clockwiseRotation = -clockwiseRotation;
+                transform.Rotate (0,0,50*Time.deltaTime);
             }
-            //Tourner auto de maxAngleRotation jusqu'à -maxAngleRotation
+            else
+            {
+                transform.rotation = Quaternion.Euler(0,0,0);
+            }
+            
         }
     }
 }
