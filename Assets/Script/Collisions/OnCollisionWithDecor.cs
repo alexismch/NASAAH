cdfb09a2d77f80606c573acd6703eb122ceceb57
amaurydetremerie <amaury.detremerie@gmail.com>
2020-11-07 00:16:47 +0100

    Ralentissement des kkhuaite dans le decor

---
+++ Assets/Script/Collisions/OnCollisionWithDecor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Script.Controllers;
+using UnityEngine;
+
+namespace Script.Collisions
+{
+    public class OnCollisionWithDecor : MonoBehaviour
+    {
+        [SerializeField] private LayerMask layerMask;
+        
+        private DynamicMovement _dynamicMovement;
+
+        private void Awake()
+        {
+            _dynamicMovement = GetComponent<DynamicMovement>();
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if ((layerMask & 1 << other.gameObject.layer) == 1 << other.gameObject.layer)
+            {
+                _dynamicMovement.SetSpeed(_dynamicMovement.Speed/2);
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if ((layerMask & 1 << other.gameObject.layer) == 1 << other.gameObject.layer)
+            {
+                _dynamicMovement.SetSpeed(_dynamicMovement.Speed*2);
+            }
+        }
+    }
+}
