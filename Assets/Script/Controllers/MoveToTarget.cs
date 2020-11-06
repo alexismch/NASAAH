56f5bf51689cbd446c4de22461e345aac831e67c
amaurydetremerie <amaury.detremerie@gmail.com>
2020-11-06 21:00:02 +0100

    Travail sur Kkhuaite Turret

--- Assets/Script/Controllers/MoveToTarget.cs
+++ Assets/Script/Controllers/MoveToTarget.cs
@@ -12,15 +12,15 @@ namespace Script.Controllers
         // Update is called once per frame
         void Update()
         {
-            if (!target)
-                target = gameManager.Player;
-            var direction = ((Vector2)(target.transform.position - gameObject.transform.position)).normalized;
-
-            //récupération de la position horizontale du player
-            var hInput = direction.x;
-            //récupération de la position verticale du player
-            var vInput = direction.y;
-            movement.Move(hInput, vInput);
+            if (target)
+            {
+                var direction = ((Vector2)(target.transform.position - gameObject.transform.position)).normalized;
+                //récupération de la position horizontale du player
+                var hInput = direction.x;
+                //récupération de la position verticale du player
+                var vInput = direction.y;
+                movement.Move(hInput, vInput);
+            }
         }
 
         public void SetTarget(GameObject newTarget)
---
+++ Assets/Script/Controllers/TurretController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Script.Controllers
+{
+    public class TurretController : MonoBehaviour
+    {
+        [SerializeField] private MoveToTarget moveToTarget;
+        [SerializeField] private GameManager.GameManager gameManager;
+        [SerializeField] private float rotationSpeed;
+        [SerializeField] private float maxAngleRotation;
+
+        private Transform _transform;
+        private int clockwiseRotation = 1;
+        private float _initialRotation;
+
+        private void Awake()
+        {
+            _transform = transform;
+            _initialRotation = _transform.rotation.z;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            moveToTarget.SetTarget(gameManager.Player);
+        }
+
+        private void Update()
+        {
+            //Définir le sens de rotation. Quand on arrive au bout, on fait clockwiseRotation = -clockwiseRotation
+            //détecter quand on est au bout de la rotation
+            
+            if (Math.Abs(_transform.rotation.z - (_initialRotation + maxAngleRotation)) == 0 || Math.Abs(_transform.rotation.z - (_initialRotation - maxAngleRotation)) == 0)
+            {
+                clockwiseRotation = -clockwiseRotation;
+            }
+            //Tourner auto de maxAngleRotation jusqu'à -maxAngleRotation
+        }
+    }
+}
