f21d6ba1b5fd27fdb6dc6afa9064f5a7e052fec9
amaurydetremerie <amaury.detremerie@gmail.com>
2020-11-06 19:14:11 +0100

    Ajout de la détection du player par la kkhuaite

--- Assets/Script/Controllers/PatrolController.cs
+++ Assets/Script/Controllers/PatrolController.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using UnityEngine;
+using Script.GameManager;
 
 namespace Script.Controllers
 {
@@ -8,25 +8,38 @@ namespace Script.Controllers
     {
         [SerializeField] private GameObject[] targetList;
         [SerializeField] private MoveToTarget moveToTarget;
+        [SerializeField] private GameManager.GameManager gameManager;
 
         private GameObject _currentTarget;
         private int _indexListTarget = 0;
+        private Transform _currentTargetTransform;
+        private Transform _selfTransform;
+        private Boolean isInPatrol = true;
 
         private void Awake()
         {
             _currentTarget = targetList[_indexListTarget];
             moveToTarget.SetTarget(_currentTarget);
+            _currentTargetTransform = _currentTarget.transform;
+            _selfTransform = gameObject.transform;
         }
 
         private void Update()
         {
-            if (((Vector2)(_currentTarget.transform.position - gameObject.transform.position)).magnitude <= 2)
+            if (isInPatrol && ((Vector2)(_currentTargetTransform.position - _selfTransform.position)).magnitude <= 2)
             {
-                _indexListTarget++;
-                _currentTarget = targetList[_indexListTarget%targetList.Length];
+                _indexListTarget = (_indexListTarget + 1)%targetList.Length;
+                _currentTarget = targetList[_indexListTarget];
                 moveToTarget.SetTarget(_currentTarget);
-
+                _currentTargetTransform = _currentTarget.transform;
             }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            Debug.Log("Hello");
+            isInPatrol = false;
+            moveToTarget.SetTarget(gameManager.Player);
         }
     }
 }
--- Assets/Script/GameManager/GameManager.cs
+++ Assets/Script/GameManager/GameManager.cs
@@ -6,7 +6,9 @@ namespace Script.GameManager
     public class GameManager : SingletonMb<GameManager>
     {
         [SerializeField] private ScoreManager scoreManager;
+        [SerializeField] private GameObject player;
         public ScoreManager ScoreManager => scoreManager;
+        public GameObject Player => player;
 
         protected override void Initialize()
         {
