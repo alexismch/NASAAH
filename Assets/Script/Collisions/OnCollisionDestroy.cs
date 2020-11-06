8bc1b9f21aaa47e79960eb87a04aa1f3c5581cc0
Huy <kh.nguyen.huy@gmail.com>
2020-11-06 18:58:10 +0100

    game manager

---
+++ Assets/Script/Collisions/OnCollisionDestroy.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+namespace Script.Collisions
+{
+    public class OnCollisionDestroy : MonoBehaviour
+    {
+
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            Debug.Log("yo");
+        }
+        
+    }
+}
---
+++ Assets/Script/Score/Score.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Script.Score
+{
+    public class Score : MonoBehaviour
+    {
+        [SerializeField] private int points;
+
+        public void ScorePoints()
+        {
+            GameManager.GameManager.Instance.ScoreManager.AddScore();
+        }
+    }
+}
