12f247d0f4cd2869bfe5b09db4d85e6670d3f2c8
Huy <kh.nguyen.huy@gmail.com>
2020-11-07 10:47:14 +0100

    d

--- Assets/Script/Object/SpawnDiamond.cs
+++ Assets/Script/Object/SpawnDiamond.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Script.Object
 {
@@ -13,18 +15,39 @@ namespace Script.Object
         // Use this for initialization
         private void Start ()
         {
-            
+            Vector3 pos;
             _spawnArea = GetComponent<BoxCollider2D>();
-            _spawnArea.enabled = false; //We don't need this to test for any collisions, just to show visual bounds info in the editor.
-            _maxSpawnPos = new Vector2(_spawnArea.size.x / 2, _spawnArea.size.y / 2);
+            _spawnArea.enabled = false;
+            var size = _spawnArea.size;
+            _maxSpawnPos = new Vector2(size.x / 2, size.y / 2);
             for(var i = 0; i< numberToSpawn;i++){
-                GameObject spawned = Instantiate(spawnedPrefab, Vector3.zero, Quaternion.identity) as GameObject;
-                spawned.transform.parent = transform;
-                Vector3 pos = new Vector3(Random.Range(-_maxSpawnPos.x, _maxSpawnPos.x), Random.Range(-_maxSpawnPos.y, _maxSpawnPos.y), 0);
-                spawned.transform.localPosition = pos;
-        
+                
+                
+                pos = new Vector3(Random.Range(-_maxSpawnPos.x, _maxSpawnPos.x),
+                        Random.Range(-_maxSpawnPos.y, _maxSpawnPos.y), 0);
+                GameObject prefab = Instantiate(spawnedPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+                prefab.transform.parent = transform;
+                prefab.transform.localPosition = pos;
+                
+                CircleCollider2D colliderPrefab = prefab.GetComponent<CircleCollider2D>();
+                colliderPrefab.enabled = false;
+                var j = 0;
+                while (j < 15 && Physics2D.OverlapBox(colliderPrefab.bounds.center, colliderPrefab.offset, 0) != null)
+                {
+                    j++;
+                    Debug.Log("'doit bouger " + i);
+                    prefab.transform.localPosition = new Vector3(Random.Range(-_maxSpawnPos.x, _maxSpawnPos.x),
+                        Random.Range(-_maxSpawnPos.y, _maxSpawnPos.y), 0);
+                }
+                colliderPrefab.enabled = true;
             }
         }
-        
+
+        private bool canSpawn(CircleCollider2D colliderPrefab )
+        {
+            bool isSpawnFree = Physics2D.OverlapBox(colliderPrefab.bounds.center, colliderPrefab.offset, 0) == null;
+            bool isNearAnotherPrefab;
+            return isSpawnFree;
+        }
     }
 }
