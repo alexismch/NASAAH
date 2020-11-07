03753a5c1f30d523f3a5d1279404bb45590263f5
alexismch <alexis.michiels@student.vinci.be>
2020-11-07 23:08:18 +0100

    Messages de découverte

--- Assets/Script/Object/DiscoverWeaponTaken.cs
+++ Assets/Script/Object/DiscoverWeaponTaken.cs
@@ -1,16 +1,20 @@
 ﻿using Script.Manager;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Script.Object
 {
     public class DiscoverWeaponTaken : WeaponTaken
     {
         [SerializeField] private GameObject diamond;
-        
+        [SerializeField] private TMP_Text text;
+
         private new void OnTriggerEnter2D(Collider2D other)
         {
             diamond.SetActive(true);
             base.OnTriggerEnter2D(other);
+            text.text = "Pour réussir les divers niveaux, vous devez ramasser 10 diamands, touvez les !";
         }
     }
 }
