ededde7c72ae90d853e189ab46b84c0a4ca36cca
alexismch <alexis.michiels@student.vinci.be>
2020-11-08 03:26:24 +0100

    add sound effects

---
+++ Assets/Script/Effects/Sound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Script.Effects
+{
+    public class Sound : MonoBehaviour
+    {
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private AudioClip audioClip;
+
+        public void StartClip()
+        {
+            audioSource.PlayOneShot(audioClip, 1f);
+            Debug.Log(audioClip);
+        }
+    }
+}
